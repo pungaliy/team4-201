a710911bf677de8bf07397b5354009acdaf6bebd
Voter <voter@usc.edu>
2018-11-06 23:18:01 -0800

    Official first Room class

--- dbsrc/db/Room.java
+++ dbsrc/db/Room.java
@@ -1,11 +1,55 @@
 package db;
 
-public class Room {
-//    int roomID;
-//    String roomStatus;
-//    arrayList<User> residents;
-//    Calendar roomCalendar;
-//    MessageBoard messageBoard;
-//    String roomImgURL;
+import java.util.ArrayList;
 
+public class Room {
+    private String roomID;
+    private String roomStatus;
+    private ArrayList<User> residents;
+    private String roomImgURL;
+
+
+    //empty constructor, for codec purposes
+    public Room() {}
+
+    public Room(String roomID, ArrayList<User> residents, String roomImgURL, String roomStatus) {
+        this.roomID = roomID;
+        this.residents = residents;
+        this.roomImgURL = roomImgURL;
+        this.roomStatus = roomStatus;
+    }
+
+
+
+    public String getRoomID() {
+        return roomID;
+    }
+
+    public void setRoomID(String roomID) {
+        this.roomID = roomID;
+    }
+
+    public String getRoomStatus() {
+        return roomStatus;
+    }
+
+    public void setRoomStatus(String roomStatus) {
+        this.roomStatus = roomStatus;
+    }
+
+    public ArrayList<User> getResidents() {
+        return residents;
+    }
+
+    public void setResidents(ArrayList<User> residents) {
+        this.residents = residents;
+    }
+
+    public String getRoomImgURL() {
+        return roomImgURL;
+    }
+
+    public void setRoomImgURL(String roomImgURL) {
+        this.roomImgURL = roomImgURL;
+    }
 }
