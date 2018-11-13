0c708082e6f6ac52726bc3cb5ec891390d96db76
kei <szekatse>
2018-11-12 22:58:05 -0800

    Try to get transaction list servlet done. Commit before merge from db

--- src/TabsServlet/TransactionList.java
+++ src/TabsServlet/TransactionList.java
@@ -1,7 +1,11 @@
 package TabsServlet;
 
 import Methods.Magic;
+import TabsStuff.TabsItem;
+import TabsStuff.TabsLedger;
 import TabsStuff.Transaction;
+import com.google.gson.Gson;
+import db.User;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,6 +13,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Vector;
 
 @WebServlet(name= "TransactionList", urlPatterns = {"/TransactionList"})
@@ -21,13 +26,26 @@ public class TransactionList extends HttpServlet{
 
 			//For testing
 			request.setAttribute("room", "5566");
+			db.User user1 = new User("user1", "id1","5566");
+			db.User user2 = new User("user2", "id2","5566");
+			db.User user3 = new User("user3", "id3","5566");
+			TabsItem fish = new TabsItem("Fish", 3, 30.0f);
+			Vector<User> split = new Vector<User>();
+			split.add(user1);
+			split.add(user2);
+			split.add(user3);
+			TabsLedger fishLedger = new TabsLedger(fish,user1, split);
 			//TODO: remove this
 
 			String room = (String) request.getAttribute("room");
 			int roomID = Integer.parseInt(room);
 
-			//Get a user here
-			//Vector<Transaction> allTransaction = magic.getAllRelatedTransaction(user);
+
+			Vector<Transaction> allTransaction = magic.getAllRelatedTransaction(user1);
+			Gson gson = new Gson();
+			String output = gson.toJson(allTransaction);
+			PrintWriter out = response.getWriter();
+			out.print(output);
 
 		}
 
