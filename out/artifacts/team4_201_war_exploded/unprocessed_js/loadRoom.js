c4dcaad08ec888b5832e0204c069b2dbfb78ad63
kei <szekatse>
2018-11-12 16:59:07 -0800

    Merge from KatrinaMessage out file

---
+++ out/artifacts/team4_201_war_exploded/unprocessed_js/loadRoom.js
@@ -0,0 +1,73 @@
+/**
+ * Gets a Promise for the Room object.
+ * @returns {*}
+ */
+function getRoomPromise() {
+    return $.get("/GetRoomServlet");
+}
+
+/**
+ * Test function. Delete later.
+ *
+ * @private
+ */
+function _print() {
+    var roomPromise = getRoomPromise();
+
+    roomPromise.success(function (room) {
+        console.log("This room's id is " + room.roomID);
+
+        for (i = 0; i < room.roomUsers.length; ++i) {
+            console.log("\t" + "User " + room.roomUsers[i].userID);
+            for (j = 0; j < room.roomUsers[i].userCalendar.sockEvents.length; ++j) {
+                var event = room.roomUsers[i].userCalendar.sockEvents[j];
+                console.log("\t\t" + "Event " + (j + 1));
+                console.log("\t\t\t" + "Summary: " + event.eventSummary);
+                console.log("\t\t\t" + "Start Date Time: " + event.startDateTime);
+                console.log("\t\t\t" + "End Date Time: " + event.endDateTime);
+            }
+        }
+
+        console.log();
+    });
+}
+
+/**
+ * Test function. Delete later.
+ * @private
+ */
+function _ajax() {
+    //JS Date objects have a month from 0-11, 10 would be November.
+    var startDateTime = new Date(2018, 10, 6, 12, 0, 0, 0);
+    var endDateTime = new Date(2018, 10, 6, 15, 0, 0, 0);
+    addEvent("Event #1", startDateTime, endDateTime);
+}
+
+/**
+ * Adds an event to the current user's calendar.
+ *
+ * @param eventSummary String that describes the event.
+ * @param startDateTime JS Date object that holds the starting date and time.
+ * @param endDateTime JS Date object that holds the ending date and time.
+ */
+function addEvent(eventSummary, startDateTime, endDateTime) {
+    var event = {
+        eventSummary: eventSummary,
+        startDateTime: {
+            year: startDateTime.getFullYear(),
+            month: startDateTime.getMonth(),
+            dayOfMonth: startDateTime.getDate(),
+            hourOfDay: startDateTime.getHours(),
+            minute: startDateTime.getMinutes()
+        },
+        endDateTime: {
+            year: endDateTime.getFullYear(),
+            month: endDateTime.getMonth(),
+            dayOfMonth: endDateTime.getDate(),
+            hourOfDay: endDateTime.getHours(),
+            minute: endDateTime.getMinutes()
+        }
+    };
+
+    $.post("/AddEventServlet", JSON.stringify(event));
+}
