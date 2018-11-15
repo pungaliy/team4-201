ce5076be74701b70f9feb24dfa48a618f3678163
Shray Pungaliya <pungaliy@usc.edu>
2018-11-14 20:54:53 -0800

    User servlet working

--- src/PostItServlets/SH5RetrievePostIts.java
+++ src/PostItServlets/SH5RetrievePostIts.java
@@ -17,10 +17,6 @@ import java.io.PrintWriter;
 public class SH5RetrievePostIts extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        User u = (User) request.getServletContext().getAttribute("user");
-        String json_str = new Gson().toJson(u);
-        System.out.println("HI" + json_str);
-        response.getWriter().print(json_str);
         String room = (String) request.getServletContext().getAttribute("room");
         NoteBase db = (NoteBase) request.getServletContext().getAttribute("notebase");
 //        NoteBase db = new NoteBase();
